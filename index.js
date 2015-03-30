baec91aa15d13b5255b20d7743ab61ce83a280b9
Zach Royer <zroyer@gmail.com>
2015-03-30 09:31:11 -0500

    work on index.js and some style added

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-index.js
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('../..')(server)
+var port = process.env.PORT || 3000
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port)
+});
+
+app.use(express.static(__dirname + '/public'))
+
+var usernames = {}
+var numUsers = 0
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  socket.on('new message', function (data) {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  socket.on('add user', function (username) {
+    socket.username = username
+    usernames[username] = username
+    ++numUsers
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      delete usernames[socket.username]
+      --numUsers;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
